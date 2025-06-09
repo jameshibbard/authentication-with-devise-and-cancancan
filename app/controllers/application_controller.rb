ef9cb700207f65244601da83ab006b90ae5dad31
James Hibbard <1940994+jameshibbard@users.noreply.github.com>
2025-06-09 14:42:13 +0200

    Customized Devise views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :name ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :name ])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   belongs_to :role
   has_many :items, dependent: :destroy
+  before_validation :assign_role
+  validates :name, presence: true
+
+  def assign_role
+    self.role = Role.find_by name: "Regular" if role.nil?
+  end
 end
